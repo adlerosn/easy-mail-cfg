754bbe874b1b4a27cfa572b78ca9a3e23f24af5c
Ádler Neves <adlerosn@gmail.com>
2018-05-11 20:36:15 -0300

    symlink for arch linux

--- easymailcfg/opendkim/configurator.py
+++ easymailcfg/opendkim/configurator.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import subprocess
 import shutil
 import re
+import os
 
 
 class OpenDkimConfigurator(Configurator):
@@ -52,6 +53,7 @@ class OpenDkimConfigurator(Configurator):
         odkim_path = Path('/etc/opendkim')
         if odkim_path.is_dir():
             shutil.rmtree(str(odkim_path))
+        os.symlink('/etc/opendkim.conf', '/etc/opendkim/opendkim.conf')
         keys_path = Path(odkim_path, 'keys')
         keys_path.mkdir(parents=True, exist_ok=True)
         trusted_hosts_file = Path(odkim_path, 'TrustedHosts')
